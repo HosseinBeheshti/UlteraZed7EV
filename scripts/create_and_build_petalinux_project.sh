21011c3bb2fceb24e7e77163301377a259b0146a
HosseinBeheshti <h.beheshti92@gmail.com>
2020-07-27 19:39:25 +0430

    remove auto configuration

--- scripts/create_and_build_petalinux_project.sh
+++ scripts/create_and_build_petalinux_project.sh
@@ -10,9 +10,9 @@ petalinux-create --type project --template zynqMP --name uz7ev
 cp ../pl/design_1_wrapper.xsa ./design_1_wrapper.xsa
 cd uz7ev
 # configure project
-petalinux-config --get-hw-description=../ --silentconfig
+petalinux-config --get-hw-description=../
 # Execute the Petalinux Build
-petalinux-build -x build_without_rm_work 
+petalinux-build
 # Package the build into Boot Images
 petalinux-package --boot --format BIN --fsbl images/linux/zynqmp_fsbl.elf --u-boot images/linux/u-boot.elf --pmufw images/linux/pmufw.elf --fpga images/linux/system.bit --force
 echo "Finished at" >> ./build_petalinux_project_runtime.txt
