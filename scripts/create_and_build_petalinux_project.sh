6cdadae6a77a0fa7154ae0061bc3d31d97d4446c
HosseinBeheshti <h.beheshti92@gmail.com>
2020-07-30 13:15:54 +0430

    add manual configuration of petalinux

--- scripts/create_and_build_petalinux_project.sh
+++ scripts/create_and_build_petalinux_project.sh
@@ -11,6 +11,42 @@ cp ../pl/design_1_wrapper.xsa ./design_1_wrapper.xsa
 cd uz7ev
 # configure project
 petalinux-config --get-hw-description=../
+# warning: current version need to change configuration manually 
+# Add aarch64 sstate-cache
+# 1) run petalinux-config
+#         -> Yocto Settings
+#            ->Local sstate feeds settings
+#                ->local sstate feeds url
+#                       (press Enter)
+# 2) Provide the path of sstate-cache from above
+#           Ex: /<path>/aarch64  for ZynqMP projects
+# 
+# Setting download mirror
+# 3) run petalinux-config
+#        -> Yocto Settings
+#           -> Add pre-mirror url
+# 	            ->  (press Enter)
+#                   Clear default value
+# 
+# 4) Provide the path of sstate-cache from above
+#       file://<path>/downloads for all projects
+# 
+# 5) run petalinux-config
+#        -> Yocto Settings
+#           -> Enable Network sstate feeds
+#              [ ] excludes
+# 
+# 6) run petalinux-config
+#        -> Yocto Settings
+#           -> Enable BB NO NETWORK
+#              [*] includes
+# 
+# 7) run petalinux-config
+#        -> Image Packaging Configuration
+#           -> Root filesystem type 
+# 	            ->  (press Enter)
+# 	                select INITRAMFS
+# 
 # Execute the Petalinux Build
 petalinux-build
 # Package the build into Boot Images
