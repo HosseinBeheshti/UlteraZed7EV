77f290edbc921e1d82d453a614ecb1186260b5c0
HosseinBeheshti <h.beheshti92@gmail.com>
2020-08-16 05:33:31 +0000

    current settings boot without rootfs config

--- scripts/build_petalinux_project.sh
+++ scripts/build_petalinux_project.sh
@@ -8,6 +8,9 @@ echo "Started at" >> $ORG_DIR/build_petalinux_project_runtime.txt
 date >> $ORG_DIR/build_petalinux_project_runtime.txt
 cd $PROJ_DIR
 
+# device tree : $ORG_DIR/petalinux/system-user.dtsi
+cp $ORG_DIR/petalinux/system-user.dtsi $PROJ_DIR/project-spec/meta-user/recipes-bsp/device-tree/files/system-user.dtsi
+
 # project configuration: $ORG_DIR/petalinux/config
 #
 # Add aarch64 sstate-cache and Setting download mirror
@@ -25,7 +28,11 @@ cd $PROJ_DIR
 # 
 # 5) run petalinux-config -> Image Packaging Configuration -> Root filesystem type -> (EXT4 (SD/eMMC/SATA/USB))
 # 
-# 6) Subsystem AUTO Hardware Settings -> Advanced bootable images storage Settings > u-boot env partition settings -> image storage media (primary sd)    
+# 6) run petalinux-config -> Image Packaging Configuration ->  (/dev/mmcblk1p2) Device node of SD device
+# 
+# 7) Subsystem AUTO Hardware Settings -> Advanced bootable images storage Settings -> u-boot env partition settings -> image storage media (primary sd)    
+# 
+# 8) Subsystem AUTO Hardware Settings -> SD/SDIO Settings -> Primary SD/SDIO (psu_sd_1) 
 cp $ORG_DIR/petalinux/config $PROJ_DIR/project-spec/configs/config
 
 # rootfs configuration: $ORG_DIR/petalinux/rootfs_config 
@@ -34,23 +41,14 @@ cp $ORG_DIR/petalinux/config $PROJ_DIR/project-spec/configs/config
 #
 # 2) Image Features -> [*] auto-login
 #
-# 4) Petalinux Package Groups -> packagegroup-petalinux -> [*] packagegroup-petalinux
+# 3) Petalinux Package Groups -> packagegroup-petalinux -> [*] packagegroup-petalinux
 #
-# 5) Petalinux Package Groups -> packagegroup-petalinux-display-debug -> [*] packagegroup-petalinux-display-debug 
+# 4) Petalinux Package Groups -> packagegroup-petalinux-multimedia -> [*] packagegroup-petalinux-multimedia  
 #
-# 6) Petalinux Package Groups -> packagegroup-petalinux-gstreamer -> [*] packagegroup-petalinux-gstreamer
+# 5) Petalinux Package Groups -> packagegroup-petalinux-openamp -> [*] packagegroup-petalinux-openamp
 #
-# 7) Petalinux Package Groups -> packagegroup-petalinux-multimedia -> [*] packagegroup-petalinux-multimedia  
+# 6) Petalinux Package Groups -> packagegroup-petalinux-v4lutils -> [*] packagegroup-petalinux-v4lutils
 #
-# 8) Petalinux Package Groups -> packagegroup-petalinux-openamp -> [*] packagegroup-petalinux-openamp
-#
-# 9) Petalinux Package Groups -> packagegroup-petalinux-python-modules -> [*] packagegroup-petalinux-python-modules
-#
-# 10) Petalinux Package Groups -> packagegroup-petalinux-qt -> [*] packagegroup-petalinux-qt
-#
-# 11) Petalinux Package Groups -> packagegroup-petalinux-v4lutils -> [*] packagegroup-petalinux-v4lutils
-#
-# 12) Petalinux Package Groups -> packagegroup-petalinux-v4lutils -> [*] packagegroup-petalinux-v4lutils
 cp $ORG_DIR/petalinux/rootfs_config $PROJ_DIR/project-spec/configs/rootfs_config
 petalinux-config -c rootfs --silentconfig 
 
@@ -64,8 +62,12 @@ petalinux-build
 petalinux-package --boot --format BIN --fsbl images/linux/zynqmp_fsbl.elf --u-boot images/linux/u-boot.elf --pmufw images/linux/pmufw.elf --fpga images/linux/system.bit --force
 echo "Finished at" >> $ORG_DIR/build_petalinux_project_runtime.txt
 date >> $ORG_DIR/build_petalinux_project_runtime.txt
-# prepare SD
-# 
-# tar xvf rootfs.tar.gz
+
+# Configuring SD boot 
+# Ref: UG1144 (v2020.1) PetaLinux Tools Documentation Reference Guide page: 76
+# $ cp images/linux/BOOT.BIN /media/BOOT/ 
+# $ cp images/linux/image.ub /media/BOOT/ 
+# $ cp images/linux/boot.scr /media/BOOT/ 
+# $ sudo tar xvf rootfs.tar.gz -C /media/rootfs
 # picocom terminal
-## sudo picocom -b 115200 -r -l /dev/ttyUSB0
+# sudo picocom -b 115200 -r -l /dev/ttyUSB0 
