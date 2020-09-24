44f2aff9a92034efc1f3d32f99208397b98c3d27
HosseinBeheshti <h.beheshti92@gmail.com>
2020-09-24 18:37:14 +0330

    start image transfer model

--- matlab/simulink/modem_phy/modelSettings.m
+++ matlab/simulink/modem_phy/modelSettings.m
@@ -1,60 +1,69 @@
+%% input transmitted image
+inputImage = imread('liftingbody.png');
+resizeImage = imresize(inputImage,[128 128]);
+vectorizedImage = resizeImage(:);
+transmitImage = zeros(1,length(vectorizedImage)/8);
+shiftVector = 256.^(0:7)';
+for i=1:length(vectorizedImage)/8
+    subsetVector = vectorizedImage(8*(i-1)+1:8*(i));
+    transmitImage(i) = sum(double(subsetVector).*shiftVector);
+end
+%%
 numHeaderBits = 16;
 %numPreambleBits = 250;
-AGCBarkerSymbols = 160+28;
+AGCBarkerSymbols = 160 + 28;
 EQTrainingSymbols = 250;
 CRCGeneratorLag = 34; % CRCLen + 2
 
 %% AGC Preamble
 B13bipolar = [0 0 0 0 0 2 2 0 0 2 0 2];
 rep = [B13bipolar, B13bipolar(1:4)];
-AGCBarker = repmat(rep,1,10);
+AGCBarker = repmat(rep, 1, 10);
 % Second preamble part
-B11bipolar = [0 0 0 2 2 2 0 2  2 0 2];
-TimingBarker = [B11bipolar,B11bipolar,B11bipolar,B11bipolar(1:6)];
+B11bipolar = [0 0 0 2 2 2 0 2 2 0 2];
+TimingBarker = [B11bipolar, B11bipolar, B11bipolar, B11bipolar(1:6)];
 PreambleBarkersInts = [AGCBarker, TimingBarker];
 % Convert integer into their bit representation
-PreambleBarkersBits = zeros(1,length(PreambleBarkersInts)*2);
-PreambleBarkersBits(1:2:end) = (PreambleBarkersInts==2);
+PreambleBarkersBits = zeros(1, length(PreambleBarkersInts) * 2);
+PreambleBarkersBits(1:2:end) = (PreambleBarkersInts == 2);
 
 % Constants
 % First preamble part
 B13bipolar = [0 0 0 0 0 2 2 0 0 2 0 2];
 rep = [B13bipolar, B13bipolar(1:4)];
-AGCBarker = repmat(rep,1,10);
+AGCBarker = repmat(rep, 1, 10);
 % Second preamble part
-B11bipolar = [0 0 0 2 2 2 0 2  2 0 2];
-TimingBarker = [B11bipolar,B11bipolar,B11bipolar(1:6)];
+B11bipolar = [0 0 0 2 2 2 0 2 2 0 2];
+TimingBarker = [B11bipolar, B11bipolar, B11bipolar(1:6)];
 PreambleBarkersInts = [AGCBarker, TimingBarker];
 % Convert integer into their bit representation
-PreambleBarkersBits = false(1,length(PreambleBarkersInts)*2);
-PreambleBarkersBits(1:2:end) = logical(PreambleBarkersInts==2);
+PreambleBarkersBits = false(1, length(PreambleBarkersInts) * 2);
+PreambleBarkersBits(1:2:end) = logical(PreambleBarkersInts == 2);
 pLen = uint16(length(PreambleBarkersBits));
-
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%
 %% RX
 %%%%%%%%%%%%%%%%%%%%%%%%%%
 
 barker = comm.BarkerCode('SamplesPerFrame', 28, 'Length', 11);
-seq = barker()+1;
+seq = barker() + 1;
 M = 4;
-hPSKModTrain   = comm.PSKModulator(M, ...
-    'PhaseOffset',pi/4, ...
-    'SymbolMapping','Binary');
+hPSKModTrain = comm.PSKModulator(M, ...
+    'PhaseOffset', pi / 4, ...
+    'SymbolMapping', 'Binary');
 xPreambleSeq = hPSKModTrain(seq);
-
 
 %%%%%%%%%%%%
 
 % AGC Preamble
 reps = 5;
 barker = comm.BarkerCode('SamplesPerFrame', 16, 'Length', 13);
-seq = barker()+1;
+seq = barker() + 1;
 %preamble = zeros(16*reps,1);
-preambleFull = repmat(seq,reps,1);
-rctFilt = comm.RaisedCosineTransmitFilter('FilterSpanInSymbols', 128,...
-'RolloffFactor',0.5,...
-'OutputSamplesPerSymbol',4);
+preambleFull = repmat(seq, reps, 1);
+rctFilt = comm.RaisedCosineTransmitFilter('FilterSpanInSymbols', 128, ...
+    'RolloffFactor', 0.5, ...
+    'OutputSamplesPerSymbol', 4);
 preambleFullFilt = rctFilt(preambleFull);
 
 %%%%%%%%%%%%
@@ -63,10 +72,10 @@ RadioSampleRate = 1e6;
 %RadioSampleRate = 20e6;
 RadioFrameLength = 1e5;
 
-VibertiTracebackLength = 34+0;
+VibertiTracebackLength = 34 + 0;
 numTrainingSamples = 250;
 crc32Poly = [1 0 0 0 0 0 1 0 0 1 1 0 0 0 0 0 1 0 0 0 1 1 1 0 1 1 0 1 1 0 1 1 1];
-crcDecodeLag = length(crc32Poly)-1+2;
+crcDecodeLag = length(crc32Poly) - 1 + 2;
 
 %%%%%%%%%%%%
 
@@ -74,48 +83,49 @@ beta = 0.5;
 span = 8;
 sps = 4;
 filterGain = 1;
-coeff = rcosdesign(beta, span, sps, 'sqrt')*filterGain; 
+coeff = rcosdesign(beta, span, sps, 'sqrt') * filterGain;
 
 %%%%%%%%%%%%
 
 samplesPerSymbol = 4;
-maxPacketBits = 8*1500;
+maxPacketBits = 8 * 1500;
 bItsPerSymbol = 2;
 CodeRate = 2;
 tailSymbols = 40;
-maxPayloadSymbols = maxPacketBits*CodeRate/bItsPerSymbol+tailSymbols;
+maxPayloadSymbols = maxPacketBits * CodeRate / bItsPerSymbol + tailSymbols;
 
 %%%%%%%%%%%%
 
 NormalizedLoopBandwidths = 0.01:0.01:1.28;
-K1 = zeros(length(NormalizedLoopBandwidths),1);
-K2 = zeros(length(NormalizedLoopBandwidths),1);
+K1 = zeros(length(NormalizedLoopBandwidths), 1);
+K2 = zeros(length(NormalizedLoopBandwidths), 1);
 
-for index=1:length(NormalizedLoopBandwidths)
-    DampingFactor = 4*1/sqrt(2);
+for index = 1:length(NormalizedLoopBandwidths)
+    DampingFactor = 4 * 1 / sqrt(2);
     samplesPerSymbolFR = 1;
     NormalizedLoopBandwidth = NormalizedLoopBandwidths(index);
-    
+
     %%
-    PhaseRecoveryLoopBandwidth = NormalizedLoopBandwidth*samplesPerSymbolFR;
-    
+    PhaseRecoveryLoopBandwidth = NormalizedLoopBandwidth * samplesPerSymbolFR;
+
     PhaseRecoveryGain = samplesPerSymbolFR;
-    
+
     PhaseErrorDetectorGain = 1;
-    
+
     %%
-    theta = PhaseRecoveryLoopBandwidth/...
-        ((DampingFactor + 0.25/DampingFactor)*samplesPerSymbolFR);
-    
-    d = 1 + 2*DampingFactor*theta + theta*theta;
-    
+    theta = PhaseRecoveryLoopBandwidth / ...
+        ((DampingFactor + 0.25 / DampingFactor) * samplesPerSymbolFR);
+
+    d = 1 + 2 * DampingFactor * theta + theta * theta;
+
     % K1
-    K1(index) = (4*DampingFactor*theta/d)/...
-        (PhaseErrorDetectorGain*PhaseRecoveryGain);
+    K1(index) = (4 * DampingFactor * theta / d) / ...
+        (PhaseErrorDetectorGain * PhaseRecoveryGain);
     % K2
-    K2(index) = (4/samplesPerSymbol*theta*theta/d)/...
-        (PhaseErrorDetectorGain*PhaseRecoveryGain);
-    
+    K2(index) = (4 / samplesPerSymbol * theta * theta / d) / ...
+        (PhaseErrorDetectorGain * PhaseRecoveryGain);
+
 end
-K1 = fi(K1,0);
-K2 = fi(K2,0);
+
+K1 = fi(K1, 0);
+K2 = fi(K2, 0);
