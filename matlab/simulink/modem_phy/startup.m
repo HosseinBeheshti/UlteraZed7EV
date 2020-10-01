ec893306244165c40b57c34f6b952d968da4b48f
HosseinBeheshti <h.beheshti92@gmail.com>
2020-10-01 21:48:41 +0330

    ready for create hdl project

--- matlab/simulink/modem_phy/startup.m
+++ matlab/simulink/modem_phy/startup.m
@@ -1,2 +1,4 @@
 addpath('./');
+hdlsetuptoolpath('ToolName','Xilinx Vivado','ToolPath','/media/fpgaws/installation/vitis/Vivado/2020.1/bin')
 uiopen('./combinedTxRx.slx',1)
+% set_param('RxTxFixedPointLibrary','Lock','off');
