275849efc040dbdd52cdf520055d1e2c49e317c7
HosseinBeheshti <h.beheshti92@gmail.com>
2020-10-14 21:50:50 +0330

    add startup files

--- matlab/simulink/modem_phy/startup.m
+++ matlab/simulink/modem_phy/startup.m
@@ -1,4 +1,6 @@
 addpath('./');
-hdlsetuptoolpath('ToolName','Xilinx Vivado','ToolPath','/media/fpgaws/installation/vitis/Vivado/2020.1/bin')
 uiopen('./combinedTxRx.slx',1)
+
+% hdlsetuptoolpath('ToolName','Xilinx Vivado','ToolPath','/media/fpgaws/installation/vitis/Vivado/2020.1/bin')
+% hdlsetuptoolpath('ToolName','Xilinx Vivado','ToolPath', 'C:\Xilinx\Vivado\2020.1\bin\vivado.bat');
 % set_param('RxTxFixedPointLibrary','Lock','off');
